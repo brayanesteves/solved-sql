9a37457f675c97fa17cf062b8ea16309834ba9fa
Brayan Esteves <besteves@moveapps.cl>
2024-03-13 06:44:25 -0400

    Efficiently Importing Large CSV Files into MySQL Database with LOAD DATA INFILE [13/03/2024]

---
+++ import/files/efficiently/index.sql
@@ -0,0 +1,3 @@
+# Efficiently Importing Large CSV Files into MySQL Database with LOAD DATA INFILE.
+# LOAD DATA FILE '<Name files>' INTO TABLE <Name table> FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+# LOAD DATA FILE 'Customers_20240313.csv' INTO TABLE customers FIELDS TERMINATED BY ',' IGNORE 1 LINES;
